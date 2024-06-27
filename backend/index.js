edb90447c082cb0dc8c522af055507b4d43c12bf
inayat bakshi <inayatbakshi75@gmail.com>
2024-06-27 20:38:14 +0530

    Add login and signup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const connection = require("./config/dbConnect");
 require('dotenv').config({path: __dirname + '/.env'});
 
 const app = express();
+app.use(express.json());
+
 const URI = process.env.DB_URI;
 const PORT = 5000;
 
@@ -15,9 +17,7 @@ connection(URI)
         console.error(error);
     });
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-})
+app.use("/api/v1/auth", require("./routes/userRoutes"));
 
 app.listen(PORT, () => {
     console.log(`Server is Running on PORT ${PORT}`);
